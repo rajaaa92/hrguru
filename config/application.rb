0861843bae3fdcd87482ade739a2c9c721938692
Kamil Paszkowski <kamil.paszkowski@netguru.pl>
2013-12-03 09:38:40 +0100

    preinitializer fix

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,10 @@
 require File.expand_path('../boot', __FILE__)
-require File.expand_path('../preinitializer', __FILE__)
 
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
 
+require File.expand_path('../preinitializer', __FILE__)
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
