5035e613ce997b12ebe726b60f363966c0a503f0
Kamil Paszkowski <kamil.paszkowski@netguru.pl>
2013-12-05 16:16:34 +0100

    fix conf for specs [#61959620]

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -2,5 +2,5 @@ require 'active_support/core_ext'
 require 'konf'
 
 pub_config = YAML.load(ERB.new(File.read('config/config.yml')).result)[Rails.env]
-sec_config = YAML.load(ERB.new(File.read('config/sec_config.yml')).result)[Rails.env] rescue {}
+sec_config = Rails.env.test? ? {} : YAML.load(ERB.new(File.read('config/sec_config.yml')).result)[Rails.env]
 AppConfig = Konf.new(pub_config.deep_merge(sec_config))
