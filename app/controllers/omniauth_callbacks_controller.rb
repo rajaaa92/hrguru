fc308838ccbb425dfb34e7393718d0593b8ce318
Kamil Paszkowski <kamil.paszkowski@netguru.pl>
2013-12-06 11:02:36 +0100

    access omniauth data via request object [#61959620]

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ class OmniauthCallbacksController <  ApplicationController
   before_filter :check_ng_person, only: :google_oauth2
 
   def google_oauth2
-    user = User.create_from_google!(env['omniauth.auth']['info'])
+    user = User.create_from_google!(request.env['omniauth.auth']['info'])
     sign_in(user)
     redirect_to root_path
   end
@@ -15,7 +15,7 @@ class OmniauthCallbacksController <  ApplicationController
   private
 
   def check_ng_person
-    if env['omniauth.auth']['extra']['raw_info']['hd'] != 'netguru.pl'
+    if request.env['omniauth.auth']['extra']['raw_info']['hd'] != 'netguru.pl'
       redirect_to root_path, error: 'No ng person'
     end
   end
