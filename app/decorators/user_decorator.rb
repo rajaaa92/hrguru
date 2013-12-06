68ecdfc95326836f5c5d4af2535d345aa30b7a04
Marcin Brzeziński <cintrzyk@gmail.com>
2013-12-06 10:51:01 +0100

    add gravatar to user [#62038126]

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -4,4 +4,9 @@ class UserDecorator < Draper::Decorator
   def name
     "#{first_name} #{last_name}"
   end
+
+  def gravatar_url(size = nil)
+    gravatar_id = Digest::MD5.hexdigest(email.downcase)
+    "http://www.gravatar.com/avatar/#{gravatar_id}?size=#{size}"
+  end
 end
