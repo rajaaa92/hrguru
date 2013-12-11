27f913158a319debfac0b5367d67c1f9d70bb30c
Marcin Brzeziński <cintrzyk@gmail.com>
2013-12-11 10:09:12 +0100

    move view logic into decorator methods [#62055584] [70b5e2d11bbb913da9e823e18b65a276af9ddc68]

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -5,8 +5,26 @@ class UserDecorator < Draper::Decorator
     "#{first_name} #{last_name}"
   end
 
+  def link
+    h.link_to name, object
+  end
+
+  def project_link
+    h.link_to(project.name, project) if project
+  end
+
   def gravatar_url(size = 80)
     gravatar_id = Digest::MD5.hexdigest(email.downcase)
     "http://www.gravatar.com/avatar/#{gravatar_id}?size=#{size}"
+  end
+
+  def gravatar_image(options = {})
+    size ||= options[:size]
+    options.delete(:size)
+    h.image_tag gravatar_url(size), options
+  end
+
+  def github_link
+    github_connected? ? h.link_to(gh_nick, "http://github.com/#{gh_nick}") : "-"
   end
 end
