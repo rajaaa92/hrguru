1765e43e128d131902402f15e4949995b3e8c074
Kamil Paszkowski <kamil.paszkowski@netguru.pl>
2013-12-12 16:34:07 +0100

    support for json [#61889472]

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,4 +1,6 @@
 class MembershipsController < ApplicationController
+  respond_to :json, only: [:create, :update]
+
   expose(:membership, attributes: :membership_params)
   expose(:memberships)
   expose(:projects) { Project.decorate }
@@ -6,18 +8,31 @@ class MembershipsController < ApplicationController
   expose(:users) { User.decorate }
 
   def create
+    membership.from ||= Date.today
     if membership.save
-      redirect_to memberships_path, notice: "Membership created!"
+      respond_to do |format|
+        format.html { redirect_to memberships_path, notice: "Membership created!" }
+        format.json { render json: membership }
+      end
     else
-      render :new
+      respond_to do |format|
+        format.html { render :new }
+        format.json { render json: { errors: membership.errors } }
+      end
     end
   end
 
   def update
     if membership.save
-      redirect_to memberships_path, alert: "Membership updated!"
+      respond_to do |format|
+        format.html { redirect_to memberships_path, alert: "Membership updated!" }
+        format.json { render json: membership }
+      end
     else
-      render :edit
+      respond_to do |format|
+        format.html { render :edit }
+        format.json { render json: { errors: membership.errors } }
+      end
     end
   end
 
