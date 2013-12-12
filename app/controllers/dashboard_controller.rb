38503644e40ce7a97815d4f9570027c548c40e18
Kamil Paszkowski <kamil.paszkowski@netguru.pl>
2013-12-12 16:31:52 +0100

    add memberships to gon [#61889472]

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,11 @@ class DashboardController < ApplicationController
   expose(:projects) { Project.all.asc(:name) }
   expose(:roles)
   expose(:users) { User.all.decorate }
+  expose(:memberships) { Membership.active }
 
   def index
     gon.rabl 'app/views/dashboard/users', as: 'users'
+    gon.rabl 'app/views/dashboard/memberships', as: 'memberships'
+    gon.currentTime = Time.now
   end
 end
---
+++ app/views/dashboard/memberships.rabl
@@ -0,0 +1,2 @@
+collection memberships
+attributes :id, :user_id, :role_id, :project_id
