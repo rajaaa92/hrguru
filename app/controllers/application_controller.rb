8c0b0772df29898c8edc5bcdf3a7ccd25387d834
Kamil Paszkowski <kamil.paszkowski@netguru.pl>
2013-12-16 10:55:43 +0100

    force people to connect github [#61889472]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :authenticate_user!
+  before_filter :connect_github
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
@@ -9,5 +10,11 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @decorated_cu ||= super.decorate if super.present?
+  end
+
+  def connect_github
+    if signed_in? && !current_user.github_connected?
+      redirect_to github_connect_path
+    end
   end
 end
