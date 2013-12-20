74a73bc3a75faa3c4d96ff574a3bfe8210edc93b
Marcin Brzeziński <cintrzyk@gmail.com>
2013-12-20 13:49:17 +0100

    add orderable module to role [#62700498]

--- app/assets/javascripts/backbone/models/role.coffee
+++ app/assets/javascripts/backbone/models/role.coffee
@@ -1,8 +1,4 @@
 class Hrguru.Models.Role extends Backbone.Model
 
-  priority: ->
-    roles = ['pm', 'qa', 'senior', 'developer', 'junior', 'praktykant']
-    roles.indexOf(@get('name')) + 1
-
 class Hrguru.Collections.Roles extends Backbone.Collection
   model: Hrguru.Models.Role
--- app/models/role.rb
+++ app/models/role.rb
@@ -2,13 +2,21 @@ class Role
   include Mongoid::Document
   include Mongoid::Timestamps
   include Mongoid::Paranoia
+  include Mongoid::Orderable
+
+  after_create :move_to_bottom
+
+  orderable column: :priority
 
   has_many :memberships
   has_many :users
 
   field :name, type: String
+  field :priority, type: Integer
 
   validates :name, presence: true
+
+  default_scope asc(:priority)
 
   def to_s
     name
