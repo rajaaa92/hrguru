643f7bdf1ee03fdd28769f102d006bf126fbc832
Kuba Łasecki <kuba.lasecki@netguru.pl>
2014-01-30 12:03:05 +0100

    env variables

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,7 +1,7 @@
 require 'rollbar/rails'
 
 Rollbar.configure do |config|
-  config.access_token = '43abef20075143f6832de97b8512eace'
+  config.access_token = ENV['SECRET_KEY_BASE']
   config.exception_level_filters.merge!('Mongoid::DocumentNotFound' => 'ignore', 'ActionController::RoutingError' => 'ignore')
 
   if Rails.env.test? || Rails.env.development?
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Hrguru::Application.config.secret_key_base = '295badce0bd702c7984846294afab07745ca5268c9bae58707664cf0c8d7d5f61e4198852edfb49f0282f7f10f2a95404915fb517395f975cd05a7eedf42ae1b'
+Hrguru::Application.config.secret_key_base = ENV['SECRET_TOKEN_KEY']
